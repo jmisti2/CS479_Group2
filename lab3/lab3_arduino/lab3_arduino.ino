1fafd86bce400429b665a015baf7260d874f443c
sergiuBuru <43687545+sergiuBuru@users.noreply.github.com>
2022-03-10 13:34:05 -0600

    Update lab3_arduino.ino
    Added code for lighting up the LEDs. Make sure to change the FSR and LED pins accordingly.

--- lab3/lab3_arduino/lab3_arduino.ino
+++ lab3/lab3_arduino/lab3_arduino.ino
@@ -1,13 +1,14 @@
 #include<Wire.h>
 /*
-A0 - LF
-A1 - HEEL
-A2 - MM
-A3 - MF
+A0 - LF   -> D3  - RED
+A1 - HEEL -> D6  - YELLOW
+A2 - MM   -> D9  - GREEN
+A3 - MF   -> D11 - BLUE
 */
 int LF, HEEL, MM, MF = 0;
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ;
+int red_timer, yellow_timer, green_timer, blue_timer;
 
 
 void setup() {
@@ -18,23 +19,62 @@ void setup() {
   Wire.write(0);    
   Wire.endTransmission(true);
   Serial.begin(115200);
+  red_timer, yellow_timer, green_timer, blue_timer = 0;
 }
 
 void loop() {
   //FSR readings
   //!!Modify according to your own wiring!!
   Serial.println("FSR");
+
+  LF = analogRead(A0);
   Serial.print("LF : ");
-  Serial.print(analogRead(A0));
+  Serial.print(LF);
 
+  HEEL = analogRead(A1);
   Serial.print(", HEEL : ");
-  Serial.print(analogRead(A1));
+  Serial.print(HEEL);
 
+  MM = analogRead(A2);
   Serial.print(", MM : ");
-  Serial.print(analogRead(A2));
+  Serial.print(MM);
 
+  MF = analogRead(A3);
   Serial.print(", MF : ");
-  Serial.println(analogRead(A3));
+  Serial.println(MF);
+  
+  //If LF is activated turn the red LED on
+  if(LF > 11) {
+    digitalWrite(3, HIGH);
+    red_timer = millis();
+  }
+  //After .3 seconds of the red LED being on, turn it off
+  if(millis() - red_timer > 250) {digitalWrite(3, LOW);}
+
+  //If HEEL is activated turn the yellow LED on
+  if(HEEL > 11) {
+    digitalWrite(6, HIGH);
+    yellow_timer = millis();
+  }
+  //After .3 seconds of the yellow LED being on, turn it off
+  if(millis() - yellow_timer > 250) {digitalWrite(6, LOW);}
+
+  //If MM is activated turn the green LED on
+  if(MM > 11) {
+    digitalWrite(9, HIGH);
+    green_timer = millis();
+  }
+  //After .3 seconds of the green LED being on, turn it off
+  if(millis() - green_timer > 250) {digitalWrite(9, LOW);}
+  
+  //If MF is activated turn the blue LED on
+  if(MF > 11) {
+    digitalWrite(11, HIGH);
+    blue_timer = millis();
+  }
+  //After .3 seconds of the blue LED being on, turn it off
+  if(millis() - blue_timer > 250) {digitalWrite(11, LOW);}
+  
 
   //Acelerometer
   Wire.beginTransmission(MPU);
@@ -49,5 +89,5 @@ void loop() {
   Serial.print(" | Y = "); Serial.print(AcY);
   Serial.print(" | Z = "); Serial.println(AcZ);  
 
-  delay(1100);
+  delay(300);
 }
