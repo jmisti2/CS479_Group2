68830fbdc3d8224f6e67a3a894206352e2d7e8a4
sergiuBuru <43687545+sergiuBuru@users.noreply.github.com>
2022-03-09 18:40:40 -0600

    Update lab3_arduino.ino
    Added functionality for reading the accelerometer values.

--- lab3/lab3_arduino/lab3_arduino.ino
+++ lab3/lab3_arduino/lab3_arduino.ino
@@ -1,4 +1,4 @@
-
+#include<Wire.h>
 /*
 A0 - LF
 A1 - HEEL
@@ -6,24 +6,48 @@ A2 - MM
 A3 - MF
 */
 int LF, HEEL, MM, MF = 0;
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ;
+
+
 void setup() {
   // put your setup code here, to run once:
-    Serial.begin(115200);
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(115200);
 }
 
 void loop() {
+  //FSR readings
   //!!Modify according to your own wiring!!
+  Serial.println("FSR");
   Serial.print("LF : ");
-  Serial.println(analogRead(A0));
+  Serial.print(analogRead(A0));
 
-  Serial.print("HEEL : ");
-  Serial.println(analogRead(A1));
+  Serial.print(", HEEL : ");
+  Serial.print(analogRead(A1));
 
-  Serial.print("MM : ");
-  Serial.println(analogRead(A2));
+  Serial.print(", MM : ");
+  Serial.print(analogRead(A2));
 
-  Serial.print("MF : ");
+  Serial.print(", MF : ");
   Serial.println(analogRead(A3));
+
+  //Acelerometer
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ);  
 
   delay(1100);
 }
